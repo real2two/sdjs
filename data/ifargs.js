04b6783d60db515a95ad977943eb52cf2f1ad267
Two <ntb.nottoday@gmail.com>
2020-03-05 20:38:26 -0500

    Create ifargs.js

---
+++ data/ifargs.js
@@ -0,0 +1,47 @@
+const Discord = require("discord.js")
+
+exports.run = (client, message, args, loopnum, maxlines) => {
+  actualargs = message.content.toString().slice(message.content.split(' ')[0].length + 1).toString()
+  if (args == "") {
+    console.log("Error on line " + loopnum.toString() + `: Improper usage of "ifargs". Must include "goto" or "else".`);
+    return
+  } else {
+    lineargs = args;
+    if (lineargs.startsWith("goto ")) {
+      lineargs = lineargs.slice(4);
+      if (lineargs == "") {
+        console.log("Error on line " + loopnum.toString() + `: Improper usage of "ifargs". No number has been input.`);
+        return
+      } else {
+        if (isNaN(parseFloat(lineargs))) {
+          console.log("Error on line " + loopnum.toString() + `: Improper usage of "ifargs". Please enter a valid number.`);
+          return
+        } else {
+          if (actualargs !== "") {
+            exports.loopnum = parseFloat(lineargs);
+          }
+          return
+        }
+      }
+    } else if (lineargs.startsWith("else ")) {
+      lineargs = lineargs.slice(5);
+      if (lineargs == "") {
+        console.log("Error on line " + loopnum.toString() + `: Improper usage of "ifargs". No number has been input.`);
+        return
+      } else {
+        if (isNaN(parseFloat(lineargs))) {
+          console.log("Error on line " + loopnum.toString() + `: Improper usage of "ifargs". Please enter a valid number.`);
+          return
+        } else {
+          if (actualargs == "") {
+            exports.loopnum = parseFloat(lineargs);
+          } else {
+            console.log("No")
+          }
+          return
+        }
+      }
+    }
+    console.log("Error on line " + loopnum.toString() + `: Improper usage of "ifargs". Must include "goto" or "else".`);
+  }
+}
