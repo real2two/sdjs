7d395e27aeb0ea0a30eddc017de21dcf39f40182
Two <ntb.nottoday@gmail.com>
2020-03-07 08:26:58 -0500

    Allowed \n to work.

--- syntax/sendembed.js
+++ syntax/sendembed.js
@@ -5,13 +5,14 @@ exports.run = (client, message, arguments, loopnum, maxlines) => {
   if (embedinfo[2]) {
     let title = embedinfo[0]
     let color = embedinfo[1]
-    let description = arguments.toString().slice(arguments.length - (arguments.toString().length - embedinfo[0].toString().length - embedinfo[1].toString().length - 6))
+    let description1 = arguments.toString().slice(arguments.length - (arguments.toString().length - embedinfo[0].toString().length - embedinfo[1].toString().length - 6))
+    let description = description1.toString().replace(/\\n/g, "\n")
     let embed = new Discord.MessageEmbed()
       .setAuthor(title)
       .setColor(color)
       .setDescription(description)
     message.channel.send({embed});
   } else {
-    console.log("Error on line " + loopnum.toString() + `: Improper usage of "send". There should be at least three arguments which are spearted with "|"s.`);
+    console.log("Error on line " + loopnum.toString() + `: Improper usage of "send". There should be at least three parameters which are spearted with "|"s.`);
   }
 }
